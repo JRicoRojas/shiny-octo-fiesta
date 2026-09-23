3200573cd60af2304fdac2bf8084e4acf5d92bf5
JRicoRojas <146977637+JRicoRojas@users.noreply.github.com>
2026-09-23 04:02:40 +0000

    Added function of randomness for array of 3 elements

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,29 @@
 console.log(document.title);
+let testArray = [256, 256, 256];
+
+
+function testCambioNumero(array){
+    let c1 = Math.floor(Math.random() * array[0]);
+    let c2 = Math.floor(Math.random() * array[1]);
+    let c3 = Math.floor(Math.random() * array[2]);
+    array [0] = c1;
+    array [1] = c2;
+    array [2] = c3;
+};
+
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+testCambioNumero(testArray)
+console.log(testArray)
+
+const test = document.getElementById("btn-test");
+test.addEventListener('click', function() {
+  console.log('El botón fue clickeado');
+});
